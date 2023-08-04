15f20dde9a3c54ca9bb02a011b7bc2091709c644
AlistairFR <jbwinckel62370@gmail.com>
2023-08-04 10:25:28 +0200

    ADD Mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+
+require('dotenv').config();
 
 const mailRoutes = require("./routes/mail");
 
 const app = express();
+
+// Gère la connexion à la base de données
+mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASS}@cluster0.xnqr89c.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 //Permet les requêtes cross-origin depuis n'importe quel domaine
 app.use(cors({ origin: "http://localhost:3000" }));
