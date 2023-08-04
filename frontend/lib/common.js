3a0a36c4cbe1e5f0b051489fb51e4fc769fc6d6b
AlistairFR <jbwinckel62370@gmail.com>
2023-08-04 14:06:01 +0200

    ADD logic Landing func for getSkills+getProjects

---
+++ frontend/lib/common.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+function formatObject(objectArray) {
+    return objectArray.map((object) => {
+      const newObject = { ...object };
+      newObject.id = newObject._id;
+      return newObject;
+    });
+}
+
+export async function getProjects() {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: 'http://localhost:4000/',
+      });
+      const projects = formatObject(response.data);
+      return projects;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+}
+
+export async function getSkills() {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: 'http://localhost:4000/',
+      });
+      const skills = formatObject(response.data);
+      return skills;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+}
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,9 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import {getProjects, getSkills} from "../lib/common";
+
 import Banner from '../components/Banner/Banner'
 import About from '../components/About/About'
 import Skills from '../components/Skills/Skills'
+import ProjectCard from '../components/Projects/ProjectCard'
 import Projects from '../components/Projects/Projects'
 
 function Main() {
+    const [projects, setProjects] = useState(null);
+    const [skills, setSkills] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const displayBooks = () => (projects ? projects.map((project) => <ProjectCard project={project} key={project._id} />) : <h1>Vide</h1>);
+    const displaySkills = () => (skills ? skills.map((skill) => <Skills skill={skill} key={skill._id} />) : <h1>Vide</h1>);
+
+    useEffect(() => {
+        async function getProjectsList() {
+          const data = await getProjects();
+          if (data) {
+            setProjects(data);
+            setLoading(false);
+          }
+        }
+        getProjectsList();
+    }, []);
+
+    useEffect(() => {
+        async function getSkillsList() {
+          const data = await getSkills();
+          if (data) {
+            setSkills(data);
+            setLoading(false);
+          }
+        }
+        getSkillsList();
+    }, []);
+
     return (
         <main className="main">
             <Banner />
