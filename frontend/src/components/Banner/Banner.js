817ffa68a0d8bdc3caf438141d7601c8ee3901d7
AlistairFR <jbwinckel62370@gmail.com>
2023-08-11 12:24:15 +0200

    ADD !NOTOK anim CSS h2

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -5,7 +5,14 @@ function Banner() {
         <div className='banner-container'>
             <div className="banner-titles">
                 <h1>Winckel Jean-Baptiste</h1>
-                <h2>Développeur Web Junior</h2>
+                <h2>
+                    <span>Développeur Web</span>
+                    <div className='message'>
+                        <div className='word1'>Junior</div>
+                        <div className='word2'>Curieux</div>
+                        <div className='word3'>Volontaire</div>
+                    </div>
+                </h2>
             </div>
             <div className="banner-button">
                 <button>COMMENCER</button>
