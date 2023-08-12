310630ea95499b8fffe8c0047feafd9d3e3c0155
AlistairFR <jbwinckel62370@gmail.com>
2023-08-12 13:34:23 +0200

    ADD gsap SkillBar component !valeur arbitraire

--- frontend/src/components/SkillBar/SkillBar.js
+++ frontend/src/components/SkillBar/SkillBar.js
@@ -1,11 +1,20 @@
+import { gsap } from 'gsap'
+
 import './SkillBar.scss'
 
 function SkillBar({ skill }) {
+    //Animation gsap de la progress bar
+    gsap.fromTo('.progress-value', {width: 0}, {
+        width: `95%`,
+        duration: 2.5,
+        ease: "power3.out",
+    });
+
     return (
         <article className='skill-container'>
             <h4>{skill.title}</h4>
             <div className='progress'>
-                <div className="progress-value">{skill.percentage}</div>
+                <div className='progress-value'>{skill.percentage}</div>
             </div>
         </article>
     )
