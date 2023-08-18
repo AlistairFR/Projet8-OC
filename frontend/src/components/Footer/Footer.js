8119ab136c8a7df18876e41fb56a56a1801ca2df
AlistairFR <jbwinckel62370@gmail.com>
2023-08-18 17:41:54 +0200

    ADD simple footer remake

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import "./Footer.scss"
 function Footer() {
     return (
         <footer>
-            <div>
-                <h3>Me contacter</h3>
+            <div className="footer-div">
+                <h3>Copyright 2023 ©️ WINCKEL Dev</h3>
                 <div className="footer-text">
-                    N'hésitez pas à me contacter via ce formulaire !
+                    
                 </div>
             </div>
         </footer>
