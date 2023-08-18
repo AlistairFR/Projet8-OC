2e60933aac16675f8f5169474f3acbe5fce25e2d
AlistairFR <jbwinckel62370@gmail.com>
2023-08-18 23:36:50 +0200

    ADD padding ProjectCard text+ about space

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -6,15 +6,15 @@ function About() {
             <div className="about-content">
                 <h3>A propos de moi</h3>
                 <p>
-                    👋 Bonjour ! Je suis Jean-Baptiste, un développeur web junior à la recherche de mon premier rôle dans le développement.
+                    👋 &nbsp;Bonjour ! Je suis Jean-Baptiste, un développeur web junior à la recherche de mon premier rôle dans le développement.
                     <br/><br/>
-                    💻 Avec une bonne maîtrise du HTML, CSS, et JavaScript, je suis passionné par la programmation, en particulier celle de jeux vidéos. Je me consacre à l'apprentissage continu des nouvelles technologies.
+                    💻 &nbsp;Avec une bonne maîtrise du HTML, CSS, et JavaScript, je suis passionné par la programmation, en particulier celle de jeux vidéos. Je me consacre à l'apprentissage continu des nouvelles technologies.
                     <br/><br/>
-                    🌟 Je suis impatient de mettre ma créativité et mes compétences au service d'une équipe. Je m'épanouis dans l'apprentissage et je suis enthousiaste à l'idée de participer à des projets qui ont un impact.
+                    🌟 &nbsp;Je suis impatient de mettre ma créativité et mes compétences au service d'une équipe. Je m'épanouis dans l'apprentissage et je suis enthousiaste à l'idée de participer à des projets qui ont un impact.
                     <br/><br/>
-                    🚀 Prêt à m'embarquer dans ce voyage, je cherche une chance d'apprendre, de grandir et de créer des expériences web significatives.
+                    🚀 &nbsp;Prêt à m'embarquer dans ce voyage, je cherche une chance d'apprendre, de grandir et de créer des expériences web significatives.
                     <br/><br/>
-                    📧 Contactez-moi et discutons ensemble !
+                    📧 &nbsp;Contactez-moi et discutons ensemble !
                 </p>
             </div>
             <div className='about-contact'>
