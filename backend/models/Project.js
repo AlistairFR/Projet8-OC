5369a8ece7125ed04ebc0291d01a1aa3f12d3e96
AlistairFR <jbwinckel62370@gmail.com>
2023-08-20 12:14:20 +0200

    ADD backend urls projects models

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -4,7 +4,9 @@ const projectSchema = mongoose.Schema({
     title : { type: String, required: true},
     desc : { type: String, required: true},
     imageUrl : { type: String, required: true},
-    descLong : { type: String, required: true}
+    descLong : { type: String, required: true},
+    repoUrl : { type: String, required: true},
+    demoUrl : { type: String, required: true}
 });
 
 module.exports = mongoose.model("projects", projectSchema);
