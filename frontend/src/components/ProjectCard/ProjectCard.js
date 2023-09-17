29548a97dabd8f67cc1d0c8a6bd24d1642d713f9
AlistairFR <jbwinckel62370@gmail.com>
2023-09-17 22:25:39 +0200

    FIX ProjectCard links clipping

--- frontend/src/components/ProjectCard/ProjectCard.js
+++ frontend/src/components/ProjectCard/ProjectCard.js
@@ -9,17 +9,18 @@ function ProjectCard({ project }) {
                 <h4>{project.title}</h4>
                 <p className='project-desc'>{project.desc}</p>
                 <div className="project-hidden">
-                    <p className='project-descLong'>{project.descLong}</p>
-                    <div className="project-links">
-                        <a href={project.repoUrl} target="_blank" rel="noreferrer">
-                            <i className="fa-solid fa-arrow-up-right-from-square" style={{color: "#00ffd5"}}/>
-                            Repo GitHub
-                        </a>
-                        <a href={project.demoUrl} target="_blank" rel="noreferrer">
-                            <i className="fa-solid fa-arrow-up-right-from-square" style={{color: "#00ffd5"}}/>
-                            Live demo
-                        </a>
-                    </div>
+                    <p className='project-descLong'>{project.descLong}
+                        <div className="project-links">
+                            <a href={project.repoUrl} target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-arrow-up-right-from-square" style={{color: "#00ffd5"}}/>
+                                Repo GitHub
+                            </a>
+                            <a href={project.demoUrl} target="_blank" rel="noreferrer">
+                                <i className="fa-solid fa-arrow-up-right-from-square" style={{color: "#00ffd5"}}/>
+                                Live demo
+                            </a>
+                        </div>
+                    </p>
                 </div>
             </div>
             <img src={imageUrl} alt={project.title} />
